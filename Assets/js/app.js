242b0798ef7abb33c856bc74890e750880252652
Aiia22 <64868714+Aiia22@users.noreply.github.com>
2021-02-05 17:10:06 +0000

    JS:  fetch login data => check if it's valid data=> store it

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -3,6 +3,11 @@ const mainNav = document.getElementById('nav1');
 const searchNav = document.getElementById('nav2');
 const register= document.getElementById('registerForm');
 const login= document.getElementById('loginForm');
+const username = document.getElementById('usernameR');
+const email = document.getElementById('emailR');
+const password = document.getElementById('passwordR');
+const passwordCheck = document.getElementById('passwordCheckR');
+
 
 //Display SearchBox
 function searchBoxNav(event){
@@ -22,9 +27,7 @@ function searchBoxNav(event){
 //Display log toggle
 function logToggle(event){
     event.preventDefault();
-    console.log("ghfdhrh");
     document.querySelector(".overlay").classList.toggle("open");
-    console.log("ok") 
 }
 
 function userLog(event){
@@ -41,3 +44,83 @@ function userLog(event){
     }
 }
 
+// Log toggle --> register
+register.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs(){
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const passwordCheckValue = passwordCheck.value.trim();
+
+    if (usernameValue === ''){
+        setErrorFor(username, 'Username is missing');
+    }else{
+        setSuccessFor(username);
+    }
+     if (emailValue === ''){
+        setErrorFor(email, 'Email is missing');
+    }else if(!isEmail(emailValue)){
+        setErrorFor(email, 'Email is not valid');
+    }
+    else{
+        setSuccessFor(email);
+    }
+    if (passwordValue === ''){
+        setErrorFor(password, 'Password is missing');
+    }else{
+        setSuccessFor(password);
+    } 
+     if (passwordCheckValue === ''){
+        setErrorFor(passwordCheck, 'Password is missing'); 
+    }else if(passwordValue !== passwordCheckValue){
+        setErrorFor(passwordCheck, 'Password does not match');
+    } 
+    else{
+        setSuccessFor(passwordCheck);
+    }
+    
+    submitInputs();
+}
+
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const errorMessage = formControl.querySelector('small');
+    errorMessage.innerText = message;
+    formControl.className = 'form-controler error';
+}
+
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-controler success'
+}
+
+function isEmail(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function submitInputs(){
+    const usernameSuccess = username.parentElement;
+    const emailSuccess = email.parentElement;
+    const passwordSuccess = password.parentElement;
+    const passwordCheckSuccess = passwordCheck.parentElement;
+
+    if(usernameSuccess.className === 'form-controler success' && emailSuccess.className === 'form-controler success' && passwordSuccess.className === 'form-controler success' && passwordCheckSuccess.className === 'form-controler success'){
+        storeInputsSignUp();
+    }
+}
+
+function storeInputsSignUp(){
+    let userData = {
+        username: document.getElementById('usernameR').value,
+        email: document.getElementById('emailR').value,
+        password: document.getElementById('passwordR').value,
+    }
+
+    localStorage.setItem('userData', JSON.stringify(userData));
+    console.log(userData);
+}
+ 
