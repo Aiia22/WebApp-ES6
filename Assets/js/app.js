93a79417e86901b0f562ca3719fb68d0a362095e
Aiia22 <64868714+Aiia22@users.noreply.github.com>
2021-02-13 12:25:29 +0000

    js/css: log toggle-->login + display username & user menu + logOut

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -7,6 +7,10 @@ const username = document.getElementById('usernameR');
 const email = document.getElementById('emailR');
 const password = document.getElementById('passwordR');
 const passwordCheck = document.getElementById('passwordCheckR');
+const usernameL = document.getElementById('usernameL');
+const emailL = document.getElementById('emailL');
+const passwordL= document.getElementById('passwordL');
+
 
 
 //Display SearchBox
@@ -44,13 +48,27 @@ function userLog(event){
     }
 }
 
+// Dipslay user menu
+ function accessMenu(event){
+    event.preventDefault();
+     if(document.querySelector(".overlayMenu").className === "overlayMenu"){
+        document.querySelector(".overlayMenu").classList.add("open");
+     }else if(document.querySelector(".overlayMenu").className === "overlayMenu close") {
+        document.querySelector(".overlayMenu ").classList.replace("close","open");
+     } else{
+          document.querySelector(".overlayMenu").classList.replace("open","close");
+         
+     }
+ }
+  
+
 // Log toggle --> register
 register.addEventListener('submit', (e)=>{
     e.preventDefault();
-    checkInputs();
+    checkInputsSignUp();
 });
 
-function checkInputs(){
+function checkInputsSignUp(){
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
@@ -82,8 +100,7 @@ function checkInputs(){
     else{
         setSuccessFor(passwordCheck);
     }
-    
-    submitInputs();
+    submitInputsSignUp();
 }
 
 function setErrorFor(input, message){
@@ -102,7 +119,7 @@ function isEmail(email){
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
-function submitInputs(){
+function submitInputsSignUp(){
     const usernameSuccess = username.parentElement;
     const emailSuccess = email.parentElement;
     const passwordSuccess = password.parentElement;
@@ -110,17 +127,112 @@ function submitInputs(){
 
     if(usernameSuccess.className === 'form-controler success' && emailSuccess.className === 'form-controler success' && passwordSuccess.className === 'form-controler success' && passwordCheckSuccess.className === 'form-controler success'){
         storeInputsSignUp();
+        //postFetchForSignUp();
     }
 }
 
-function storeInputsSignUp(){
-    let userData = {
+ function storeInputsSignUp(){
+    let userDataSignUp = {
         username: document.getElementById('usernameR').value,
         email: document.getElementById('emailR').value,
         password: document.getElementById('passwordR').value,
     }
-
-    localStorage.setItem('userData', JSON.stringify(userData));
-    console.log(userData);
+    localStorage.setItem('userDataSignUp', JSON.stringify(userDataSignUp));
+    DisplayUsername(document.getElementById('usernameR').value);
 }
+
+/*  function postFetchForSignUp() {
+  let usernameL = document.getElementById("usernameL")
+  let emailL = document.getElementById("emailL")
+  let passwordL = document.getElementById("passwordL")
+  
+  fetch('http://localhost:3000/users', { 
+    method: 'POST',
+    headers: {
+      'Content-Type':'Assets/JSON/application.json',
+      'Accept':'Assets/JSON/application.json',
+    },
+    body: JSON.stringify({
+      usernameLValue: usernameL.value,
+      emailLValue: emailL.value,
+      passwordLValue: passwordL.value
+    })
+  })
+  .then(res=>res.json())
+  .then(user => {
+    localStorage.clear() // If user signed in => clear it up                                 
+                         
+    localStorage.id = user.id  // Then store the id 
+    logOut()
+  })
+} */ 
+
+
+// Log toggle --> login
+login.addEventListener('submit', (e)=>{
+   e.preventDefault();
+    validateForm();
+});
+
+function validateForm(){
+    const emailLValue = emailL.value.trim();
+    const passwordLValue = passwordL.value.trim();
+    const userData = JSON.parse(window.localStorage.getItem('userDataSignUp'));
+    console.log(userData);
+    const email = userData.email.trim();
+    const username = userData.username.trim();
+    console.log(username);
+    const password = userData.password.trim();
+    
+    if(emailLValue  === email && passwordLValue === password){
+        DisplayUsername(username)
+    }else{
+        alert("Login was unsuccessful, please check your email or password");
+        return false;
+    }
+}
+
+/* signInButton.addEventListener('click', e => {
+  signIn()
+  let form = signDiv.querySelector('.sign-in')
+  let usernameInput = document.querySelector("#username")
+  form.addEventListener('submit', e=>{
+    e.preventDefault()
+    fetch('http://localhost:3000/users') // We make a get fetch    // request where users are stored
+    .then(res=>res.json()) 
+    .then(usersArray => { 
+      let user = usersArray.find(function(user){ 
+          return user.username === usernameInput.value // Then we        // check if there is a user with a value given
+        })
+      if (user){
+        signDiv.innerHTML = ""
+        localStorage.id = user.id // If there is so, we then store // it
+        logOut()
+      }
+    })
+  })
+}) */
+
+
+// Log toggle --> login/register (common functions)
+ function DisplayUsername(username){
+    const name = username;
+    if (name !== 'undefined'|| name !== 'null'){
+        document.getElementById('user').innerHTML = name;
+    }else{
+        document.getElementById('user').innerHTML = "Hello!";
+        console.log('azert');
+    }
+   document.querySelector(".fa-caret-down").classList.add("visible");
+   document.querySelector(".overlay").classList.remove("open");
+}
+
+function logOut(){
+    document.getElementById('user').innerHTML = "";
+    document.querySelector(".fa-caret-down").classList.remove("visible");
+    document.querySelector(".overlayMenu").classList.replace("open","close")
+    //localStorage.clear() // clear localStorage 
+}
+
  
+
